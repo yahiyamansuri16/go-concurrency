eb082384ea3be142593ccc636132440daee4fa00
yahiya mansuri <y.mansuri@arkoselabs.com>
2025-08-02 18:58:33 +0530

    wait group example added

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 func main() {
-	
+	var wg sync.WaitGroup
+	wg.Add(1) // Add 1 goroutine to wait for
+	go func() {
+		defer wg.Done() // Signal completion when this goroutine finishes
+		defer wg.Done()
+		fmt.Println("Hello from GoRoutine")
+	}()
+	wg.Wait() // wait for goroutines to complete
 }
